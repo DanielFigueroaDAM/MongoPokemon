223e41e831605273e802a30ca08f8c7e833b4184
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-12-15 13:48:32 +0100

    añadidos los repositorio

--- src/main/java/org/example/repository/AdestradorRepository.java
+++ src/main/java/org/example/repository/AdestradorRepository.java
@@ -1,4 +1,9 @@
 package org.example.repository;
 
-public interface AdestradorRepository {
+import org.example.model.Adestrador;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdestradorRepository extends MongoRepository<Adestrador, String> {
 }
