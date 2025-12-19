10b4b123dd1c7cf4ab9d009786a84f5f7ce5575c
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-12-19 13:39:52 +0100

    cambios termnados los services de momento

--- src/main/java/org/example/service/AdestradorService.java
+++ src/main/java/org/example/service/AdestradorService.java
@@ -1,4 +1,28 @@
 package org.example.service;
 
+import org.example.model.Adestrador;
+import org.example.repository.AdestradorRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class AdestradorService {
+
+    private final AdestradorRepository adestradorRepo;
+
+    public AdestradorService(AdestradorRepository adestradorRepo) {
+        this.adestradorRepo = adestradorRepo;
+    }
+
+    public void crearAdestrador(Adestrador a) {
+        adestradorRepo.save(a);
+    }
+
+    public Adestrador buscarAdestrador(String id) {
+        return adestradorRepo.findById(id).orElse(null);
+    }
+    // Buscar Adestrador
+    public List<Adestrador> buscarAdestradores() {
+        return adestradorRepo.findAll();
+    }
 }
--- src/main/java/org/example/service/PokemonService.java
+++ src/main/java/org/example/service/PokemonService.java
@@ -1,9 +1,42 @@
 package org.example.service;
 
 import org.example.model.Adestrador;
+import org.example.model.Pokemon;
+import org.example.repository.AdestradorRepository;
+import org.example.repository.PokemonRepository;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-public class PokemonService {
+import java.util.List;
 
+public class PokemonService {
+    private final PokemonRepository pokemonRepo;
+    private final AdestradorRepository adestradorRepo;
+
+    public PokemonService(PokemonRepository pokemonRepo, AdestradorRepository adestradorRepo) {
+        this.pokemonRepo = pokemonRepo;
+        this.adestradorRepo = adestradorRepo;
+    }
+
+    public void crearPokemon(Pokemon a) {
+        pokemonRepo.save(a);
+    }
+
+    public Pokemon buscarPokemon(String id) {
+        return pokemonRepo.findById(id).orElse(null);
+    }
+    // Buscar pokemon
+    public List<Pokemon> buscarPokemons() {
+        return pokemonRepo.findAll();
+    }
+
+    // Buscar adestradorDepokemon
+    public Adestrador buscarAdestradorDepokemon(String idPokemon) {
+        Pokemon pokemon = buscarPokemon(idPokemon);
+        if (pokemon == null) return null;
+
+        Adestrador adestrador = adestradorRepo.findById(pokemon.getAdestradorId()).orElse(null);
+
+        return adestrador;
+    }
 }
